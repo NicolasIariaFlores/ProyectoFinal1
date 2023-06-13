fb35c59d26d7b293eb3816b691666a202c9d89ff
Nicolas Iaria Flores <nicolasiaria99@gmail.com>
2023-06-13 02:49:23 -0300

    first commit

---
+++ project/apps/home/urls.py
@@ -0,0 +1,11 @@
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path("", views.index, name="index"),
+    path("login/", views.login_request, name="login")
+]
+
+urlpatterns += staticfiles_urlpatterns
---
+++ project/apps/home/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from django.http import HttpRequest, HttpResponse
+
+# * Para el login
+from django.contrib.auth import authenticate, login
+from django.contrib.auth.forms import AuthenticationForm
+
+def index(request: HttpRequest) -> HttpResponse:
+    return render(request, "home/index.html")
+
+def login_request(request):
+    if request.method == "POST":
+        form = AuthenticationForm(request.POST)
+        if form.is_valid():
+            usuario = form.cleaned_data.get("username")
+            contraseña = form.cleaned_data.get("password")
+            user = authenticate(username=usuario, password=contraseña)
+            if user is not None: 
+                login(request, user)
+                return render(request, "home/index.html", {"mensaje" : f"¡Bienvenido {usuario}!"})
+    else: 
+        form = AuthenticationForm()
+    return render(request, "home/login.html", {"form":form})
+
